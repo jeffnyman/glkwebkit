f3e3b55da6151614375bfd9e6f0a5ce09593811c
Jeff Nyman <jeffnyman@gmail.com>
2023-09-19 01:28:40 -0500

    Adding primary modules.

---
+++ src/glkjs.js
@@ -0,0 +1,6 @@
+export class GlkJS {
+  constructor() {
+    this.name = "GlkJS";
+    this.version = "0.1.0";
+  }
+}
---
+++ src/webglk.js
@@ -0,0 +1,6 @@
+export class WebGlk {
+  constructor() {
+    this.name = "WebGlk";
+    this.version = "0.1.0";
+  }
+}
